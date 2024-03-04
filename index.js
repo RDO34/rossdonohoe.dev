5abf684c42008746e5447de314d4caffdd30f469
RDO34 <rossdon.95@googlemail.com>
2024-03-04 18:56:25 +0000

    impl alias, cat, echo, improve cmd, welcome

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const state = {
   path: "~",
+  env: {},
+  date: new Date(),
 };
 
 const dirMap = {
@@ -26,6 +28,10 @@ addEventListener("keydown", function (e) {
 
   if (e.key === "Backspace") {
     deletePrevChar();
+  }
+
+  if (e.key === "Delete") {
+    deleteCurrentChar();
   }
 
   if (e.key === "ArrowLeft") {
@@ -55,6 +61,17 @@ function deletePrevChar() {
 
   if (prev) {
     parentNode.removeChild(prev);
+  }
+}
+
+function deleteCurrentChar() {
+  const parentNode = caret().parentNode;
+  const next = caret().nextElementSibling;
+  const current = caret();
+
+  if (current) {
+    parentNode.removeChild(current);
+    next.classList.add("caret");
   }
 }
 
@@ -124,10 +141,20 @@ function makeLeaderText() {
   return `guest@rossdonohoe.dev:${state.path}$`;
 }
 
+function println(text) {
+  const newLine = document.createElement("span");
+  newLine.classList.add("terminal-line");
+  newLine.innerHTML = text;
+  document.querySelector(".terminal").appendChild(newLine);
+}
+
 const handlers = {
+  alias: alias,
   ls: list,
+  cat: concatenate,
   cd: changeDirectory,
   clear: clear,
+  echo: echo,
   chroot: permissionDenied("chroot"),
   su: permissionDenied("su"),
   sudo: permissionDenied("sudo"),
@@ -143,9 +170,13 @@ const HTML_SPACE_CHAR = " ";
 
 function handleCommand() {
   const commands = document.querySelectorAll(".terminal-input");
-
   const command = commands[commands.length - 1].textContent.trim();
+  if (command.length > 0) {
+    cmd(command);
+  }
+}
 
+function cmd(command) {
   const [baseCommand, ...args] = command.split(HTML_SPACE_CHAR);
 
   if (handlers[baseCommand]) {
@@ -156,18 +187,12 @@ function handleCommand() {
 }
 
 function insertCommandNotFound(command) {
-  const newLine = document.createElement("span");
-  newLine.classList.add("terminal-line");
-  newLine.innerHTML = `sh: command not found: ${command}`;
-  document.querySelector(".terminal").appendChild(newLine);
+  println(`sh: command not found: ${command}`);
 }
 
 function permissionDenied(command) {
   return function () {
-    const newLine = document.createElement("span");
-    newLine.classList.add("terminal-line");
-    newLine.innerHTML = `sh: ${command}: permission denied`;
-    document.querySelector(".terminal").appendChild(newLine);
+    println(`sh: ${command}: permission denied`);
   };
 }
 
@@ -177,14 +202,15 @@ function clear() {
 }
 
 function list(args) {
-  const dirPathParts = state.path.split("/");
-  let currentDir = dirMap[dirPathParts.pop()];
+  const pathArg = args.find((arg) => !arg.startsWith("-"));
+  const dir = Path.resolve(pathArg);
 
-  while (dirPathParts.length > 0) {
-    currentDir = dirMap[currentDir];
+  if (typeof dir === "string") {
+    println(`ls: cannot access '${pathArg}': No such file or directory`);
+    return;
   }
 
-  let entries = [".", "..", ...Object.keys(currentDir)];
+  let entries = [".", "..", ...Object.keys(dir)];
 
   const includeHidden = args.some(
     (arg) => arg.startsWith("-") && arg.includes("a")
@@ -192,6 +218,40 @@ function list(args) {
 
   if (!includeHidden) {
     entries = entries.filter((entry) => !entry.startsWith("."));
+  }
+
+  const longFormat = args.some(
+    (arg) => arg.startsWith("-") && arg.includes("l")
+  );
+
+  if (longFormat) {
+    const entryDetails = entries.map((entry) => {
+      const isDir = typeof dir[entry] === "object";
+      const typeAndPermissions = `${isDir ? "d" : "-"}r--r--r--`;
+      const links = 1;
+      const owner = "guest";
+      const group = "guest";
+      const size = isDir ? 4096 : 1024;
+
+      const month = state.date.toLocaleString("default", { month: "short" });
+      const day = state.date.getDate();
+      const time = state.date.toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
+      const date = `${month} ${day} ${time}`;
+
+      return [typeAndPermissions, links, owner, group, size, date, entry].join(
+        HTML_SPACE_CHAR
+      );
+    });
+
+    for (const entry of entryDetails) {
+      println(entry);
+    }
+
+    return;
   }
 
   const newLine = document.createElement("span");
@@ -235,3 +295,115 @@ function changeDirectory(args) {
     state.path = newPath;
   }
 }
+
+function echo(args) {
+  println(args.join(HTML_SPACE_CHAR));
+}
+
+function alias(args) {
+  const [alias, command] = args[0].split("=");
+
+  if (!alias || !command) {
+    for (const arg of args.filter((arg) => !arg.startsWith("-"))) {
+      println(`sh: alias: ${arg}: not found`);
+    }
+    return;
+  }
+
+  handlers[alias] = function (args) {
+    cmd([command, ...args].join(HTML_SPACE_CHAR).trim());
+  };
+}
+
+// cat
+function concatenate(args) {
+  const filePath = args[0];
+
+  const file = Path.resolve(filePath);
+
+  if (typeof file === "string") {
+    println(file);
+  } else {
+    println(`cat: ${filePath}: No such file or directory`);
+  }
+}
+
+class Path {
+  static join(...args) {
+    return args.join("/");
+  }
+
+  static resolve(_path) {
+    const dirPathParts = state.path.split("/").filter(Boolean);
+
+    if (_path) {
+      const upTimes = (_path.match(/\.\./g) || []).length;
+      dirPathParts.splice(-upTimes, upTimes);
+    }
+
+    let currentDir = dirMap[dirPathParts.shift()];
+
+    while (dirPathParts.length > 0) {
+      currentDir = currentDir[dirPathParts.shift()];
+    }
+
+    if (!_path) {
+      return currentDir;
+    }
+
+    let path = _path;
+
+    if (path.startsWith("./")) {
+      path = path.slice(2);
+    }
+
+    while (path.startsWith("../")) {
+      path = path.slice(3);
+    }
+
+    const pathParts = path.split("/").filter(Boolean);
+    let resolved = currentDir[pathParts.shift()];
+    for (const part of pathParts) {
+      resolved = resolved[part];
+    }
+
+    return resolved;
+  }
+}
+
+async function simulateTyping(text) {
+  for (const char of text) {
+    inputChar(char);
+    await sleep();
+  }
+}
+
+async function sleep(_ms) {
+  let ms = _ms;
+
+  if (!ms) {
+    ms = Math.floor(Math.random() * 100) + 50;
+  }
+
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+const scipts = {
+  welcome,
+};
+
+async function welcome() {
+  await simulateTyping("echo Welcome to rossdonohoe.dev!");
+  await sleep();
+  handleCommand();
+  createNewLine();
+
+  await sleep(400);
+
+  await simulateTyping("ls -l");
+  await sleep();
+  handleCommand();
+  createNewLine();
+}
+
+scipts.welcome();
